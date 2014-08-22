313e65f9e8c4aa91e3a4b4f0135b5ec444817958
s-tajima <tajima1989@gmail.com>
2014-08-22 16:01:15 +0000

    fix scripts

--- manage_config_file/manage_config_file.rb
+++ manage_config_file/manage_config_file.rb
@@ -5,7 +5,7 @@ command     = ARGV.shift
 config_yaml = ARGV.shift
 config_yaml ||= "./manage_config_file.yml"
 
-available_commands = ["gather", "push", "extract"]
+available_commands = ["gather", "push", "clone", "extract"]
 exit 1 unless available_commands.include?(command)
 
 `ruby ./manage_config_file_#{command}.rb #{config_yaml}`
--- manage_config_file/manage_config_file_libs.rb
+++ manage_config_file/manage_config_file_libs.rb
@@ -1,3 +1,7 @@
+#
+# gather
+#
+
 def gather(path, dest_dir, dry_run = false)
   unless File.exists?(path)
     warn "[Skip] #{path} doesn't exists." 
@@ -26,7 +30,51 @@ def gather_file(path, dest_dir)
   puts "[OK] #{path} gathered."
 end
 
-
 def gather_directory(path, dest_dir)
   gather_file(path, dest_dir)
+end
+
+#
+# extract
+#
+def extract(path, dest_dir, dry_run = false)
+  unless File.exists?(path)
+    warn "[Skip] #{path} doesn't exists." 
+    return false
+  end
+  type = File.ftype(path)
+
+  return extract_file(path, dest_dir)      if type == "file"
+  return extract_directory(path, dest_dir) if type == "directory"
+
+  warn "[Skip] #{path} unexpected file type." 
+  return false
+end
+
+def extract_file(path, dest_dir)
+  unless File.delete(path)
+    warn "[Skip] #{path} remove file failed." 
+    return
+  end
+
+  unless File.symlink("#{dest_dir}/#{File.basename(path)}", path) 
+    warn "[Skip] #{path} create symlink failed." 
+    return
+  end
+
+  puts "[OK] #{path} extracted."
+end
+
+def extract_directory(path, dest_dir)
+  unless FileUtils.rm_rf(path)
+    warn "[Skip] #{path} remove directory failed." 
+    return
+  end
+
+  unless File.symlink("#{dest_dir}/#{File.basename(path)}", path) 
+    warn "[Skip] #{path} create symlink failed." 
+    return
+  end
+
+  puts "[OK] #{path} extracted."
 end
