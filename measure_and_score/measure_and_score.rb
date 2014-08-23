af00d88e4e91800f09309cf4957dec29acec989d
s-tajima <tajima1989@gmail.com>
2014-08-23 03:20:18 +0000

    add yml sample

--- measure_and_score/measure_and_score.rb
+++ measure_and_score/measure_and_score.rb
@@ -4,8 +4,17 @@ require 'yaml'
 config_yaml = ARGV.shift
 config_yaml ||= "./measure_and_score.yml"
 
-
 configs = YAML.load_file(config_yaml)
 
-pp configs[:measure_command]
-puts `#{configs[:measure_command]}`
+unless File.exists?("#{configs[:config_dir]}/.git")
+  warn "[Fail] #{configs[:config_dir]} doesn't git dir." 
+  exit 1
+end
+
+puts result = `#{configs[:measure_command]}`
+
+Dir.chdir(configs[:config_dir])
+`git add -A`
+`git commit -m "#{result}"`
+`git push`
+
