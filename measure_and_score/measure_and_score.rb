ad735ec79420c8624bc26b508cb76c968c8cece3
s-tajima <tajima1989@gmail.com>
2014-08-24 08:38:36 +0000

    fix measure_and_score.rb

--- measure_and_score/measure_and_score.rb
+++ measure_and_score/measure_and_score.rb
@@ -1,33 +1,12 @@
 require 'pp'
 require 'yaml'
 
+command     = ARGV.shift
 config_yaml = ARGV.shift
 config_yaml ||= "./measure_and_score.yml"
 
-configs = YAML.load_file(config_yaml)
+available_commands = ["run", "push", "revert"]
+exit 1 unless available_commands.include?(command)
 
-unless File.exists?("#{configs[:config_dir]}/.git")
-  warn "[Fail] #{configs[:config_dir]} doesn't git dir." 
-  exit 1
-end
-
-Dir.chdir(configs[:config_dir])
-
-configs[:before_commands].each do |command|
-  puts `#{command}`
-end
-
-result = `#{configs[:measure_command]}`
-
-configs[:after_commands].each do |command|
-  puts `#{command}`
-end
-
-Dir.mkdir("./gather_files/") unless Dir.exists?("./gather_files/")
-configs[:gather_target].each do |target|
-  puts `cp -r #{target} #{configs[:config_dir]}/gather_files/`
-end
-
-`git add -A`
-`git commit -m '#{result}'`
+`ruby ./measure_and_score_#{command}.rb #{config_yaml}`
 
---
+++ measure_and_score/measure_and_score_push.rb
@@ -0,0 +1,16 @@
+require 'pp'
+require 'yaml'
+
+config_yaml = ARGV.shift
+config_yaml ||= "./measure_and_score.yml"
+
+configs = YAML.load_file(config_yaml)
+
+unless File.exists?("#{configs[:config_dir]}/.git")
+  warn "[Fail] #{configs[:config_dir]} doesn't git dir." 
+  exit 1
+end
+
+Dir.chdir(configs[:config_dir])
+
+`git push`
